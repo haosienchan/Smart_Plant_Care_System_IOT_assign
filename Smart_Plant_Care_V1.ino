844bca3db0ed2b11ba08bbed76e78a1ce63511d3
Chan Hao Sien <chansien92324@gmail.com>
2026-05-26 21:47:21 +0800

    integrate dht22 climate monitoring and ldr ambient light sensing

--- Smart_Plant_Care_V1.ino
+++ Smart_Plant_Care_V1.ino
@@ -2,12 +2,23 @@
 #include <Arduino_MQTT_Client.h>
 #include <PubSubClient.h>
 #include <ThingsBoard.h>
+#include <DHT.h>
 
+
+#define LDR_PIN           35
+#define DHT_PIN           4
+#define DHTTYPE           DHT22
 const char* WIFI_SSID = "jack";
 const char* WIFI_PASSWORD = "1912Cx30";
 const char* TOKEN = "zdmMMCbwdnGnUAfIQgif";
 const char* THINGSBOARD_SERVER = "thingsboard.cloud";
 constexpr uint16_t THINGSBOARD_PORT = 1883;
+
+DHT dht(DHT_PIN, DHTTYPE);
+
+int lightThreshold = 2000;
+float tempThreshold = 40.0;
+float humidityThreshold = 80.0;
 
 WiFiClient wifiClient;
 Arduino_MQTT_Client mqttClient(wifiClient);
