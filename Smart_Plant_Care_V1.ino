3a7d7b6259979aba75eee5cbd8cc4c40960603b3
Chan Hao Sien <chansien92324@gmail.com>
2026-05-26 21:48:27 +0800

    implement irrigation control with water-level safety interlock

--- Smart_Plant_Care_V1.ino
+++ Smart_Plant_Care_V1.ino
@@ -4,7 +4,9 @@
 #include <ThingsBoard.h>
 #include <DHT.h>
 
-
+#define SOIL_PIN          34
+#define WATER_LEVEL_PIN   32
+#define RELAY_PIN         5
 #define LDR_PIN           35
 #define DHT_PIN           4
 #define DHTTYPE           DHT22
@@ -19,6 +21,50 @@ DHT dht(DHT_PIN, DHTTYPE);
 int lightThreshold = 2000;
 float tempThreshold = 40.0;
 float humidityThreshold = 80.0;
+int soilThreshold = 2300;
+bool pumpOn = false;
+
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  dht.begin();
+
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH); // Safety off default
+  pinMode(WATER_LEVEL_PIN, INPUT);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) connectWiFi();
+  if (!tb.connected()) connectThingsBoard();
+
+  int waterState = digitalRead(WATER_LEVEL_PIN);
+  bool lowWater = false;
+  int water = 1;
+  int pump = 0;
+
+  if (waterState == HIGH) {
+    Serial.println("Low Water!");
+    lowWater = true;
+    water = 0;
+    digitalWrite(RELAY_PIN, HIGH);
+    pumpOn = false;
+  }
+
+  int soilValue = analogRead(SOIL_PIN);
+
+  if (!lowWater) {
+    if (soilValue > soilThreshold) {
+      digitalWrite(RELAY_PIN, LOW);
+      pumpOn = true;
+      pump = 1;
+    } else {
+      digitalWrite(RELAY_PIN, HIGH);
+      pumpOn = false;
+      pump = 0;
+    }
+  }
 
 WiFiClient wifiClient;
 Arduino_MQTT_Client mqttClient(wifiClient);
